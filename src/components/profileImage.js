550d8b97d21688933e90f079104dcc94cf985910
Dennis Lee <dennis-lcl@users.noreply.github.com>
2019-07-11 16:21:14 +0800

    Change profile picture.

--- src/components/profileImage.js
+++ src/components/profileImage.js
@@ -3,12 +3,8 @@ import React from "react";
 const profileImage = () => {
   return (
     <React.Fragment>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj7ofc1wowg9RdK8onw5mVGY63hvrZxQXUlnRWBZ9lBWMbJdQ-8Q"
-        alt="This is me."
-      />
+      <img src="https://i.redd.it/aessvr740kq01.jpg" alt="This is me." />
     </React.Fragment>
   );
 };
-
 export default profileImage;
