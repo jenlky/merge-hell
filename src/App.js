ca137ee36b8d39798d4cbac16bece1b2672dede7
john <johnwee35@gmail.com>
2019-07-11 16:27:01 +0800

    profile background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Profile from "./components/profile";
+import Profile from "./components/profile/profile";
 
 function App() {
   return (
