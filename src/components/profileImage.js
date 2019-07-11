1a2988df6bc492a6f04013b6c939fd5ab4b52eeb
Dennis Lee <dennis-lcl@users.noreply.github.com>
2019-07-11 15:53:07 +0800

    Create profileImage component.

---
+++ src/components/profileImage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const profileImage = () => {
+  return (
+    <React.Fragment>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj7ofc1wowg9RdK8onw5mVGY63hvrZxQXUlnRWBZ9lBWMbJdQ-8Q"
+        alt="This is me."
+      />
+    </React.Fragment>
+  );
+};
+
+export default profileImage;
