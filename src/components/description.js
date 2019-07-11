e49aa35a0b0080ac7c1e4db6289e897eafd40bda
Shalinidea <Shalinidealmeida@gmail.com>
2019-07-11 16:16:03 +0800

    Add Name and Description Components

---
+++ src/components/description.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Description = () => {
+  return <div>Hi I'm from England and live in a castle. Like my pictures!</div>;
+};
+
+export default Description;
---
+++ src/components/name.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Name = () => {
+  return <div>Elizabeth Regina</div>;
+};
+
+export default Name;
