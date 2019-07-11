42125a2f43bb8eb0c75f980fd4b5088ff4f43475
n4t0b0t <natalieong1990@users.noreply.github.com>
2019-07-11 16:41:12 +0800

    adds single star

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./components/profile";
+import Stars from "./Stars";
 
 function App() {
   return (
     <div className="App">
+      <Stars />
       <header className="App-header">
         <Profile />
       </header>
